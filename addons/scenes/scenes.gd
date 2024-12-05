96a77bf51db16756a73275bec09d0ffea189228d
dusk <y.bera003.06@protonmail.com>
2024-12-06 01:15:49 +0300

    docs: minor

--- addons/scenes/scenes.gd
+++ addons/scenes/scenes.gd
@@ -20,13 +20,13 @@ func _ready() -> void:
 	change_started.emit(cur_scene.scene_file_path, _params)
 	# if playing a specific scene
 	if ProjectSettings.get("application/run/main_scene") != cur_scene.scene_file_path:
-		# call pre_start and start method to ensure compatibility with "Play Scene"
 		if not cur_scene.is_node_ready():
 			await cur_scene.ready
 		scene_loaded.emit(cur_scene, _params)
 	scene_changed.emit(cur_scene, _params)
 
 
+# fully sets the new scene up
 func _set_new_scene(resource: PackedScene) -> void:
 	var current_scene = get_tree().current_scene
 	current_scene.queue_free()
